961b0cbd3b61d505a5462de76f310ba1252a5625
Ernesto Júnior <ernesto.sjunior@hotmail.com>
2021-02-28 22:09:47 -0300

    Update

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {Button, TextInput, View, Text } from 'react-native';
+import {css} from './assets/css/Css'
 
 export default function App() {
+
+    const [altura, setAltura]=useState(0);
+    const [peso, setPeso]=useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    
+    <View style={css.container}>
+      <TextInput
+      placeholder="Insira sua altura"
+      altura={altura}/>
+      <TextInput
+      placeholder="Insira seu peso"
+      peso={peso}/>
+
+      <Button title='Caclular IMC' onClick={()=>setAltura()}/>
+      <Text>Sua altura é {altura}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ assets/css/Css.js
@@ -0,0 +1,14 @@
+import { StyleSheet} from 'react-native';
+
+const css = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#8c52ff',
+      alignItems: 'center',
+      justifyContent: 'center'
+     
+    },
+   
+  });
+  
+export {css};
